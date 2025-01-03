a7530062dd8724e2487ae27cf8dea3f36b449e54
mucahidkoc23 <mucahidkoc.tr@gmail.com>
2025-01-03 15:08:57 +0300

    add pug and set up backend-database integration

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const { passangerDatabase } = require("./database");
+const flatted = require("flatted");
 const app = express();
 
+app.set("view engine", "pug");
+
+app.get("/passengers", async (req, res) => {
+  const passengers = await passangerDatabase.load();
+  // res.send(flatted.stringify(passengers));
+  res.render(`passengers`, { passengers });
+});
+
+app.get("/passengers/:passengerId", async (req, res) => {
+  const passenger = await passangerDatabase.find(req.params.passengerId);
+  if (!passenger) return res.status(404).send("Passenger not found");
+  res.render(`passenger`, { passenger });
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render(`index`);
 });
 
 app.listen(3000, () => {
