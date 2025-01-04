cfd84d17d71ab67e2b9bf756ce49621b460e8eba
mucahidkoc23 <mucahidkoc.tr@gmail.com>
2025-01-04 20:32:36 +0300

    add routes

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
+const passengersRouter = require("./routes/passengers");
+const indexRouter = require("./routes/index");
 const express = require("express");
-const { passangerDatabase } = require("./database");
-const flatted = require("flatted");
 const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
 app.set("view engine", "pug");
 
-app.get("/passengers", async (req, res) => {
-  const passengers = await passangerDatabase.load();
-  // res.send(flatted.stringify(passengers));
-  res.render(`passengers`, { passengers });
-});
+app.use("/passengers", passengersRouter);
 
-app.post("/passengers", async (req, res) => {
-  const passenger = await passangerDatabase.insert(req.body);
-  res.send(passenger);
-});
-
-app.delete("/passengers/:passengerId", async (req, res) => {
-  await passangerDatabase.removeBy("id", req.params.passengerId);
-  res.send("Passenger removed");
-});
-
-app.get("/passengers/:passengerId", async (req, res) => {
-  const passenger = await passangerDatabase.find(req.params.passengerId);
-  if (!passenger) return res.status(404).send("Passenger not found");
-  res.render(`passenger`, { passenger });
-});
-
-app.get("/", (req, res) => {
-  res.render(`index`);
-});
+app.use("/", indexRouter);
 
 app.listen(3000, () => {
   console.log("Started listening port 3000");
---
+++ routes/passengers.js
@@ -0,0 +1,40 @@
+const { passangerDatabase, driverDatabase } = require("../database");
+const flatted = require("flatted");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  const passengers = await passangerDatabase.load();
+  // res.send(flatted.stringify(passengers));
+  res.render(`passengers`, { passengers });
+});
+
+router.post("/", async (req, res) => {
+  const passenger = await passangerDatabase.insert(req.body);
+  res.send(passenger);
+});
+
+router.delete("/:passengerId", async (req, res) => {
+  await passangerDatabase.removeBy("id", req.params.passengerId);
+  res.send("Passenger removed");
+});
+
+router.get("/:passengerId", async (req, res) => {
+  const passenger = await passangerDatabase.find(req.params.passengerId);
+  if (!passenger) return res.status(404).send("Passenger not found");
+  res.render(`passenger`, { passenger });
+});
+
+router.get("/:passengerId/bookings", async (req, res) => {
+  const { driverId, origin, destination } = req.body;
+  const { passengerId } = req.params;
+
+  const passenger = await passangerDatabase.find(passengerId);
+  const driver = await driverDatabase.find(driverId);
+
+  passenger.book(driver, origin, destination);
+  await passangerDatabase.update(passenger);
+  res.send(flatted.stringify(passenger));
+});
+
+module.exports = router;
